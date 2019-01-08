947b9516f6553d7156b019c11927efd63dbf17f5
Trinopoty Biswas <trinopoty@hotmail.com>
2019-01-08 14:55:18 +0530

    Suppressed jetty logging

--- engine.io-server-test/src/test/java/io/socket/engineio/server/ServerWrapper.java
+++ engine.io-server-test/src/test/java/io/socket/engineio/server/ServerWrapper.java
@@ -6,6 +6,8 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.log.Log;
+import org.eclipse.jetty.util.log.Logger;
 import org.eclipse.jetty.websocket.server.WebSocketUpgradeFilter;
 
 import javax.servlet.ServletException;
@@ -23,13 +25,14 @@ final class ServerWrapper {
     private Server mServer;
     private EngineIoServer mEngineIoServer;
 
+    static {
+        Log.setLog(new JettyNoLogging());
+    }
+
     ServerWrapper() {
         mPort = PORT_START.getAndIncrement();
         mServer = new Server(mPort);
         mEngineIoServer = new EngineIoServer();
-
-        System.setProperty("org.eclipse.jetty.util.log.class", "org.eclipse.jetty.util.log.StdErrLog");
-        System.setProperty("org.eclipse.jetty.LEVEL", "OFF");
 
         ServletContextHandler servletContextHandler = new ServletContextHandler(ServletContextHandler.SESSIONS);
         servletContextHandler.setContextPath("/");
@@ -69,5 +72,70 @@ final class ServerWrapper {
 
     EngineIoServer getEngineIoServer() {
         return mEngineIoServer;
+    }
+
+    private static final class JettyNoLogging implements Logger {
+
+        @Override public String getName() {
+            return "no";
+        }
+
+        @Override
+        public void warn(String s, Object... objects) {
+        }
+
+        @Override
+        public void warn(Throwable throwable) {
+        }
+
+        @Override
+        public void warn(String s, Throwable throwable) {
+        }
+
+        @Override
+        public void info(String s, Object... objects) {
+        }
+
+        @Override
+        public void info(Throwable throwable) {
+        }
+
+        @Override
+        public void info(String s, Throwable throwable) {
+        }
+
+        @Override
+        public boolean isDebugEnabled() {
+            return false;
+        }
+
+        @Override
+        public void setDebugEnabled(boolean b) {
+        }
+
+        @Override
+        public void debug(String s, Object... objects) {
+        }
+
+        @Override
+        public void debug(String s, long l) {
+        }
+
+        @Override
+        public void debug(Throwable throwable) {
+        }
+
+        @Override
+        public void debug(String s, Throwable throwable) {
+        }
+
+        @Override
+        public Logger getLogger(String s) {
+            return this;
+        }
+
+        @Override
+        public void ignore(Throwable throwable) {
+        }
     }
 }
