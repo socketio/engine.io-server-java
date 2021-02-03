bfce2e88fa9879d6b283e7315d77077f8a3e0d29
Trinopoty Biswas <trinopoty@hotmail.com>
2021-02-03 18:38:42 +0530

    Updated ServerYeast to use SecureRandom to minimize risk of spoofing

--- engine.io-server/src/main/java/io/socket/yeast/ServerYeast.java
+++ engine.io-server/src/main/java/io/socket/yeast/ServerYeast.java
@@ -1,12 +1,22 @@
 package io.socket.yeast;
 
-import java.util.concurrent.atomic.AtomicLong;
+import java.security.SecureRandom;
 
 public final class ServerYeast {
 
-    private static final AtomicLong VALUE = new AtomicLong(System.currentTimeMillis());
+    /**
+     * A ThreadLocal is used to improve thread performance since {@link SecureRandom} do not perform well with
+     * thread contention.
+     */
+    private static final ThreadLocal<SecureRandom> THREAD_RANDOM = new ThreadLocal<>();
 
     public static String yeast() {
-        return Yeast.encode(VALUE.incrementAndGet());
+        SecureRandom secureRandom = THREAD_RANDOM.get();
+        if (secureRandom == null) {
+            secureRandom = new SecureRandom();
+            THREAD_RANDOM.set(secureRandom);
+        }
+
+        return Yeast.encode(secureRandom.nextLong() & 0x7fffffffffffffffL);
     }
 }
